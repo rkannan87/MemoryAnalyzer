f7261e73744dc9879d776104bb8c7592f785e319
rkannan87 <65522806+rkannan87@users.noreply.github.com>
2022-10-04 13:24:21 -0400

    Initial commit

---
+++ mat/ParseHeapDump.bat
@@ -0,0 +1,13 @@
+@echo off
+REM 
+REM This script parses a heap dump.
+REM
+REM Usage: ParseHeapDump.bat <path/to/dump.hprof> [report]*
+REM 
+REM The leak report has the id org.eclipse.mat.api:suspects
+REM 
+
+set _DIRNAME=.\
+if "%OS%" == "Windows_NT" set _DIRNAME=%~dp0%
+
+"%_DIRNAME%\eclipsec.exe" --launcher.ini "%_DIRNAME%\MemoryAnalyzer.ini" -consoleLog -application org.eclipse.mat.api.parse %*
